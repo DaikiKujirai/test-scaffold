e27f2cc1d09b530d19bed3beadc9106b5d5fff99
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-03-25 15:23:18 +0900

    182

--- udemy-vuejs2/src/App.vue
+++ udemy-vuejs2/src/App.vue
@@ -3,20 +3,29 @@
     <button @click="myAnimation = 'slide'">Slide</button>
     <button @click="myAnimation = 'fade'">Fade</button>
     <p>{{myAnimation}}</p>
+    <br>
+    <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <transition-group name="fade">
+        <li
+          style="cursor: pointer;"
+          v-for="(number, index) in numbers"
+          :key="number"
+          @click="remove(index)"
+          >{{number}}</li>
+      </transition-group>
+    </ul>
+    <br>
     <button @click="show = !show">切り替え</button>
-    <transion>
-      <div class="circle" v-if="show"></div>
-    </transion
+    <br><br>
+    <transition
+      :css="false"
       @before-enter="beforeEnter"
       @enter="enter"
-      @after-enter="afterEnter"
-      @enter-cancelled="enterCanselled"
-
-      @before-leave="beforeLeave"
       @leave="leave"
-      @after-leave="afterLeave"
-      @leave-cancelled="leaveCanselled"
-      >
+    >
+      <div class="circle" v-if="show"></div>
+    </transition>
     <button @click="myComponent = 'ComponentA'">ComponentA</button>
     <button @click="myComponent = 'ComponentB'">ComponentB</button>
     <transition name="fade" mode="out-in">
@@ -30,7 +39,7 @@
       enter-active-class="animate__animated animate__bounce"
       leave-active-class="animate__animated animate__shakeX"
     >
-    <p v-if="show">hello</p>
+      <p v-if="show">hello</p>
     </transition>
     <transition
     :name="myAnimation"
@@ -52,20 +61,49 @@ export default {
   },
   data() {
     return {
+      numbers: [0, 1, 2],
+      nextNumber: 3,
       show: true,
       myAnimation: "slide",
       myComponent: "ComponentA"
     };
   },
   methods: {
-    beforeEnter(el) {},
-    enter(el, done) {},
-    afterEnter(el) {},
-    enterCancelled(el) {},
-    beforeLeave(el) {},
-    leave(el, done) {},
-    afterLeave(el) {},
-    leaveCancelled(el) {},
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el) {
+      el.style.transform = 'scale(0)'
+    },
+    enter(el, done) {
+      let scale = 0;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1
+        if ( scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    leave(el, done) {
+      let scale = 1;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1
+        if ( scale < 0) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
   }
 };
 </script>
@@ -79,11 +117,14 @@ export default {
   background-color: deeppink;
 }
 
+.fade-move {
+  transition: transform 1s;
+}
 .fade-enter {
   opacity: 0;
 }
 .fade-enter-active {
-  transition: opacity 0.5s;
+  transition: opacity 1s;
 }
 .fade-enter-to {
   opacity: 1;
@@ -92,7 +133,9 @@ export default {
   opacity: 1;
 }
 .fade-leave-active {
-  transition: opacity 0.5s;
+  transition: opacity 1s;
+  position: absolute;
+  width: 200px;
 }
 .fade-leave-to {
   opacity: 0;
@@ -125,4 +168,4 @@ export default {
   padding-top: 5rem;
   text-align: center;
 }
-</style>script
+</style>
