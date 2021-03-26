35115410c41cde346e4c464aa62d4baf3ce2f443
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-03-26 21:40:56 +0900

    completed

--- vuejs-http/src/axios-refresh.js
+++ vuejs-http/src/axios-refresh.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const instance = axios.create({
   baseURL:
-    "https://identitytoolkit.googleapis.com/v1"
+    "https://securetoken.googleapis.com/v1"
+
 })
 
 export default instance;
